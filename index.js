b91b9ee98c3b43a431bd8e329859bf7331c54325
RobinkumarJ246 <robin246j@gmail.com>
2024-03-21 16:29:55 +0530

    Welcome email

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const nodemailer = require('nodemailer');
 
 const app = express();
 const cors = require('cors');
@@ -34,6 +35,49 @@ app.post('/api/register', async (req, res) => {
   } finally {
     await client.close();
   }
+});
+
+// Nodemailer transporter setup
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'innovatexcel.team@gmail.com', // Your email address
+    pass: 'bbyw zbva omrb tche', // Your email password or app-specific password if using Gmail
+  },
+});
+
+// Function to send custom email
+const sendCustomEmail = (toEmail, subject, text) => {
+  // Email options
+  const mailOptions = {
+    from: 'innovatexcel.team@gmail.com', // Sender address
+    to: toEmail, // Recipient address
+    subject: subject, // Subject line
+    html:htmlContent, // Plain text body
+  };
+
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+    } else {
+      console.log('Email sent:', info.response);
+    }
+  });
+};
+
+// Define route for sending email
+app.post('/api/welcome-mail', (req, res) => {
+  const { toEmail, subject, htmlContent } = req.body;
+
+  // Send the email
+  sendCustomEmail(toEmail, subject, htmlContent);
+
+  // Respond with a success message
+  res.status(200).json({ message: 'Email sent successfully' });
 });
 
 const PORT = process.env.PORT || 3000; // Use environment variable or fallback to 3000
